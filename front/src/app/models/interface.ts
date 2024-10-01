dab189a1b3607203322a0a3807a89e1fe2865ba9
mrx_light <pmalanici@gmail.com>
2024-10-01 21:52:55 +0300

    GG_02 Develop page Profile. Get everything for future pages

--- front/src/app/models/interface.ts
+++ front/src/app/models/interface.ts
@@ -7,3 +7,182 @@ export interface UserInterface {
   confirmPassword?: string;
   isActive?: boolean;
 }
+
+export interface LeaguesInterface {
+  country: {
+    code: null | number | string;
+    flag: null | string;
+    name: string;
+  };
+  league: {
+    id: number;
+    logo: string | null;
+    name: string;
+    type: string;
+  };
+  seasons: {
+    coverage: any;
+    current: boolean | null;
+    end: string | null;
+    start: string | null;
+    year: number;
+  }[];
+}
+
+export interface TeamsInterface {
+  team: {
+    country: string;
+    code: string;
+    founded: number;
+    name: string;
+    logo: string;
+    national: string;
+    id: number;
+  };
+  venue: {
+    image: string | null;
+    address: string;
+    surface: string;
+    city: string;
+    name: string;
+    id: number;
+    capacity: number;
+  };
+}
+
+export interface CoachesInterface {
+  age: number;
+  birth: {
+    country: string;
+    date: string;
+    place: string | null;
+  };
+  career: {
+    end: string | null;
+    start: string | null;
+    team: {
+      id: number;
+      logo: string;
+      name: string;
+    }[];
+  }[];
+  firstname: string;
+  height: string | number | null;
+  id: number;
+  lastname: string;
+  name: string;
+  nationality: string | null;
+  photo: string;
+  team: {
+    id: number;
+    logo: string;
+    name: string;
+  };
+  weight: number | string | null;
+}
+
+export interface PlayerInterface {
+  player: {
+    age: number;
+    birth: {
+      date: string;
+      place: string | null;
+      country: string;
+    };
+    firstname: string;
+    height: string | null;
+    id: number;
+    injured: boolean;
+    lastname: string;
+    name: string;
+    nationality: string;
+    photo: string | null;
+    weight: string | number | null;
+  };
+  statistics: {
+    cards: {
+      yellow: number | null;
+      yellowred: number | null;
+      red: number | null;
+    };
+    dribbles: {
+      attempts: null | number | string;
+      success: null | number | string;
+      past: null | number | string;
+    };
+    duel: {
+      total: null | number | string;
+      success: null | number | string;
+      past: null | number | string;
+    };
+    fouls: {
+      drown: null | number | string;
+      committed: null | number | string;
+    };
+    games: {
+      appearences: null | number | string;
+      captain: boolean;
+      lineups: number;
+      minutes: number;
+      number: number | null;
+      position: string | null;
+      rating: null | number | string;
+    };
+    goals: {
+      total: number | null;
+      conceded: string | null;
+      assists: number | null;
+      saves: null;
+    };
+    league: {
+      country: string;
+      flag: string | null;
+      id: number;
+      logo: string | null;
+      name: string;
+      season: string;
+    };
+    passes: {
+      total: null | number | string;
+      key: null | number | string;
+      accuracy: null | number | string;
+    };
+    penalty: {
+      commited: null | number | string;
+      missed: null | number | string;
+      saved: null | number | string;
+      scored: null | number | string;
+      won: null | number | string;
+    };
+    shots: {
+      total: null | number | string;
+      on: null | number | string;
+    };
+    substitutes: {
+      in: null | number;
+      out: null | number;
+      bench: null | number;
+    };
+    tackles: {
+      total: null | number;
+      blocks: null | number;
+      interceptions: null | number;
+    };
+    team: {
+      id: number;
+      name: string | null;
+      logo: string | null;
+    };
+  }[];
+}
+
+export interface VenuesInterface {
+  address: string | null;
+  capacity: number | null;
+  city: string;
+  country: string;
+  id: number;
+  image: string | null;
+  name: string;
+  surface: string | null;
+}
--- front/src/app/pages/profile/profile.component.ts
+++ front/src/app/pages/profile/profile.component.ts
@@ -1,19 +1,126 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { PassDataService } from '../../services/pass-data.service';
 import { NgOptimizedImage } from '@angular/common';
+import { ProfileService } from '../../services/profile.service';
+import { LeaguesCardComponent } from '../../components/leagues-card/leagues-card.component';
+import { TeamCardComponent } from '../../components/team-card/team-card.component';
+import { CoachCardComponent } from '../../components/coach-card/coach-card.component';
+import { PlayerCardComponent } from '../../components/player-card/player-card.component';
+import { VenueCardComponent } from '../../components/venue-card/venue-card.component';
+import { ErrorHandlerService } from '../../services/error-handler.service';
+import { Subject, takeUntil } from 'rxjs';
+import { MenuComponent } from '../../layouts/menu/menu.component';
+import { HeaderComponent } from '../../layouts/header/header.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [
+    NgOptimizedImage,
+    LeaguesCardComponent,
+    TeamCardComponent,
+    CoachCardComponent,
+    PlayerCardComponent,
+    VenueCardComponent,
+    MenuComponent,
+    HeaderComponent,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
-export class ProfileComponent implements OnInit {
-  passUser = inject(PassDataService);
-  imgUrl = './../../../assets/img/banner.png';
+export class ProfileComponent implements OnInit, OnDestroy {
+  passData = inject(PassDataService);
+  profileServices = inject(ProfileService);
+  errorServices = inject(ErrorHandlerService);
+  private ngUnsubscribe = new Subject<void>();
+  limitedLeagues = [];
+  limitedTeams = [];
+  limitedCoach = [];
+  limitedPlayers = [];
+  limitedVenues = [];
   ngOnInit() {
-    const getUser = this.passUser.getUser.source._value;
-    console.log('Profile User:', getUser);
+    // const getUser = this.passData.getUser.source._value;
+    this.profileServices
+      .getLeagues()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: (res: any) => {
+          this.passData.setLeagues(res.response);
+          const getLeagues = this.passData.getLeagues.source._value;
+          this.limitedLeagues = getLeagues.slice(0, 5);
+        },
+        error: (err) => {
+          this.errorServices.errorHandleProfile(err);
+        },
+      });
+    this.profileServices
+      .getTeams()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: (res: any) => {
+          this.passData.setTeams(res.response);
+          const getTeams = this.passData.getTeams.source._value;
+          this.limitedTeams = getTeams.slice(0, 5);
+        },
+        error: (err) => {
+          this.errorServices.errorHandleProfile(err);
+        },
+      });
+    this.profileServices
+      .getCouches()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: (res: any) => {
+          this.passData.setCoach(res.response);
+          const getCoach = this.passData.getCoachs.source._value;
+          if (getCoach.length > 5) {
+            this.limitedCoach = getCoach.slice(0, 5);
+            return;
+          }
+          this.limitedCoach = getCoach;
+        },
+        error: (err) => {
+          this.errorServices.errorHandleProfile(err);
+        },
+      });
+    this.profileServices
+      .getPlayers()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: (res: any) => {
+          this.passData.setPlayers(res.response);
+          const getPlayers = this.passData.getPlayers.source._value;
+          if (getPlayers.length > 5) {
+            this.limitedPlayers = getPlayers.slice(0, 5);
+            return;
+          }
+          this.limitedPlayers = getPlayers;
+        },
+        error: (err) => {
+          this.errorServices.errorHandleProfile(err);
+        },
+      });
+    this.profileServices
+      .getVenues()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: (res: any) => {
+          this.passData.setVenues(res.response);
+          const getVenues = this.passData.getVenues.source._value;
+          if (getVenues.length > 5) {
+            this.limitedVenues = getVenues.slice(0, 5);
+            return;
+          }
+          this.limitedVenues = getVenues;
+        },
+        error: (err) => {
+          this.errorServices.errorHandleProfile(err);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
